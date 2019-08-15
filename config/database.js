efa4043fa512349a4aea05a50dc2e1d78268d431
ChavyChaze <sobolrip1@gmail.com>
2019-08-16 00:17:40 +0300

    Start working with Docker, add remote DB

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 const Sequeliza = require('sequelize');
 
-module.exports = new Sequeliza(process.env.MYSQL_DB_NAME, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+// module.exports = new Sequeliza(process.env.MYSQL_DB_NAME, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+//   dialect: 'mysql',
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+
+const db = new Sequeliza('sql7301704', 'sql7301704', 'NUVrEyxiQj', {
   dialect: 'mysql',
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-    
-    // const db = new Sequeliza('sql7301009', 'sql7301009', '7C2jVnKNtY', {
-    //     dialect: 'postgres',
-    //     host: 'sql7.freemysqlhosting.net',
-    //     port: 3306,
-  
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-  });
+  host: 'sql7.freemysqlhosting.net',
+  port: 3306,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
