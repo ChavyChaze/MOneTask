0fa6a46c5c08f3cc6827069327b161dc1e140049
ChavyChaze <sobolrip1@gmail.com>
2019-08-16 00:59:02 +0300

    finish with setting up docker, and docker-composer

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,10 @@ COPY package.json ./
 
 RUN npm install
 
+# RUN npm audit fix
+
 COPY . .
 
 EXPOSE 8080
 
-CMD ["npm", ""]
+CMD ["npm", "start"]
--- config/database.js
+++ config/database.js
@@ -1,14 +1,9 @@
 const Sequeliza = require('sequelize');
 
-// module.exports = new Sequeliza(process.env.MYSQL_DB_NAME, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-//   dialect: 'mysql',
-//   host: process.env.MYSQL_HOST,
-//   port: process.env.MYSQL_PORT,
-
-const db = new Sequeliza('sql7301704', 'sql7301704', 'NUVrEyxiQj', {
+module.exports = new Sequeliza(process.env.MYSQL_DB_NAME, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   dialect: 'mysql',
-  host: 'sql7.freemysqlhosting.net',
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
 
   pool: {
     max: 5,
@@ -17,5 +12,3 @@ const db = new Sequeliza('sql7301704', 'sql7301704', 'NUVrEyxiQj', {
     idle: 10000
   }
 });
-
-module.exports = db;
